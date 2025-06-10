beec91bd55faa67d29fb0d1bb5c69823e1926498
Alex Felici <sandroms521@gmail.com>
2025-06-10 00:28:42 -0400

    switch pages

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,14 @@ app.get('/', (req: any, res: any) => {
   res.sendFile(path.join(__dirname, './static/home/index.html'));
 });
 
+// TODO how to use router
+// TODO how to get username/password
+// TODO how to include account functions
+app.post('/account', (req, res) => {
+  const { username, password } = req.body;
+  res.render('the_template', { username, password });
+});
+
 // Global error handler (should be after routes)
 app.use(errorHandler);
 
